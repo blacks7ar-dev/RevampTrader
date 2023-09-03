6fa808f3ecf7c3d4bb678fe2fb1e01c183726bfa
blacks7ar <129362030+blacks7ar-dev@users.noreply.github.com>
2023-09-03 19:41:55 +0800

    added sellable yml configuration

---
+++ RevampTrader/Functions/SellableItem.cs
@@ -0,0 +1,17 @@
+﻿using YamlDotNet.Serialization;
+
+namespace RevampTrader.Functions;
+
+public struct SellableItem
+{
+    [YamlMember(Alias = "Prefab", ApplyNamingConventions = false)]
+    public string m_prefab { get; set; }
+    [YamlMember(Alias = "Value", ApplyNamingConventions = false)]
+    public int m_value { get; set; }
+
+    public SellableItem()
+    {
+        m_prefab = null;
+        m_value = 0;
+    }
+}
--- RevampTrader/Plugin.cs
+++ RevampTrader/Plugin.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
@@ -20,11 +22,13 @@ namespace RevampTrader
         private const string modGUID = "blacks7ar.RevampTrader";
         public const string modName = "RevampTrader";
         public const string modAuthor = "blacks7ar";
-        public const string modVersion = "1.0.1";
+        public const string modVersion = "1.0.2";
         public const string modLink = "https://valheim.thunderstore.io/package/blacks7ar/RevampedTrader/";
-        private static string traderYml = modGUID + ".yml";
+        private static string buyYaml = modGUID + ".Buy.yml";
         private static readonly string configPath = Paths.ConfigPath;
-        private static string fullYmlPath = Paths.ConfigPath + Path.DirectorySeparatorChar + traderYml;
+        private static string fullBuyYamlPath = Paths.ConfigPath + Path.DirectorySeparatorChar + buyYaml;
+        private static string sellYaml = modGUID + ".Sell.yml";
+        private static string fullSellYamlPath = Paths.ConfigPath + Path.DirectorySeparatorChar + sellYaml;
         public static readonly ManualLogSource RTLogger = BepInEx.Logging.Logger.CreateLogSource(modName);
         private static readonly Harmony _harmony = new(modGUID);
         
@@ -37,10 +41,14 @@ namespace RevampTrader
         
         private static ConfigEntry<Toggle> _serverConfigLocked;
 
-        private static readonly CustomSyncedValue<List<TraderYaml>> _TraderYml = new(_configSync, "Trader Items",
+        private static readonly CustomSyncedValue<List<TraderYaml>> _BuyYaml = new(_configSync, "Trader Items",
             new List<TraderYaml>());
 
+        private static readonly CustomSyncedValue<List<SellableItem>> _SellYaml = new(_configSync, "Player Items",
+            new List<SellableItem>());
+
         private static List<TraderYaml> _traderItem { get; set; }
+        private static List<SellableItem> _playerItem { get; set; }
         public static string connectionError = "";
 
         private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
@@ -59,27 +67,50 @@ namespace RevampTrader
         
         private void TraderWatcher()
         {
-            var watcher = new FileSystemWatcher(configPath, traderYml);
-            watcher.Changed += ReadTraderYaml;
-            watcher.Created += ReadTraderYaml;
-            watcher.Renamed += ReadTraderYaml;
+            var watcher = new FileSystemWatcher(configPath, buyYaml);
+            watcher.Changed += ReadBuyYaml;
+            watcher.Created += ReadBuyYaml;
+            watcher.Renamed += ReadBuyYaml;
             watcher.IncludeSubdirectories = true;
             watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
             watcher.EnableRaisingEvents = true;
+            var watcher2 = new FileSystemWatcher(configPath, sellYaml);
+            watcher2.Changed += ReadSellYaml;
+            watcher2.Created += ReadSellYaml;
+            watcher2.Renamed += ReadSellYaml;
+            watcher2.IncludeSubdirectories = true;
+            watcher2.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher2.EnableRaisingEvents = true;
         }
 
-        private void ReadTraderYaml(object sender, FileSystemEventArgs e)
+        private void ReadSellYaml(object sender, FileSystemEventArgs e)
         {
             try
             {
-                var streamReader = File.OpenText(fullYmlPath);
-                _traderItem = ReadSerializedYml(streamReader.ReadToEnd());
+                var streamReader = File.OpenText(fullSellYamlPath);
+                _playerItem = ReadSerializedSellYml(streamReader.ReadToEnd());
                 streamReader.Close();
-                _TraderYml.AssignLocalValue(_traderItem);
+                _SellYaml.AssignLocalValue(_playerItem);
             }
             catch
             {
-                Logging.LogError($"There was an issue loading you {traderYml}");
+                Logging.LogError($"There was an issue loading you {sellYaml}");
+                Logging.LogError("Please check your entries for spelling and format!");
+            }
+        }
+
+        private void ReadBuyYaml(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                var streamReader = File.OpenText(fullBuyYamlPath);
+                _traderItem = ReadSerializedBuyYml(streamReader.ReadToEnd());
+                streamReader.Close();
+                _BuyYaml.AssignLocalValue(_traderItem);
+            }
+            catch
+            {
+                Logging.LogError($"There was an issue loading you {buyYaml}");
                 Logging.LogError("Please check your entries for spelling and format!");
             }
         }
@@ -91,18 +122,39 @@ namespace RevampTrader
             _configSync.AddLockingConfigEntry(_serverConfigLocked);
             var assembly = Assembly.GetExecutingAssembly();
             _harmony.PatchAll(assembly);
-            if (!File.Exists(fullYmlPath))
+            if (!File.Exists(fullBuyYamlPath))
             {
-                CreateTraderYml();
-                Logging.LogWarning($"File not found - {traderYml}!");
+                CreateBuyYml();
+                Logging.LogWarning($"File not found - {buyYaml}!");
                 Logging.LogWarning("Creating a new one..");
             }
-            ReadTraderYaml(null, null);
-            _TraderYml.ValueChanged += TraderYmlOnValueChanged;
+            ReadBuyYaml(null, null);
+            _BuyYaml.ValueChanged += BuyYamlOnValueChanged;
+            if (!File.Exists(fullSellYamlPath))
+            {
+                CreateSellYml();
+                Logging.LogWarning($"File not found - {sellYaml}!");
+                Logging.LogWarning("Creating a new one..");
+            }
+            ReadSellYaml(null, null);
+            _SellYaml.ValueChanged += SellYamlOnValueChanged;
             TraderWatcher();
         }
 
-        private static void TraderYmlOnValueChanged()
+        private static void SellYamlOnValueChanged()
+        {
+            foreach (var prefab in ObjectDB.instance.m_items)
+            {
+                foreach (var item in _playerItem.Where(item => prefab.name == item.m_prefab))
+                {
+                    prefab.GetComponent<ItemDrop>().m_itemData.m_shared.m_value = item.m_value;
+                }
+            }
+            ObjectDB.instance.UpdateItemHashes();
+            Logging.LogDebug($"{sellYaml} changed..");
+        }
+
+        private static void BuyYamlOnValueChanged()
         {
             var array = Resources.FindObjectsOfTypeAll<GameObject>();
             foreach (var gameObject in array)
@@ -122,31 +174,58 @@ namespace RevampTrader
                     });
                 }
             }
-            Logging.LogDebug($"{traderYml} changed..");
+            Logging.LogDebug($"{buyYaml} changed..");
         }
 
         private void OnDestroy()
         {
             _harmony.UnpatchSelf();
         }
-        
-        [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.SetupLocations))]
+
+        [HarmonyPatch]
         public static class Patches
         {
-            public static void Postfix()
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.SetupLocations))]
+            public static void SetupLocations_Postfix()
             {
-                _TraderYml.ValueChanged += TraderYmlOnValueChanged;
-                TraderYmlOnValueChanged();
+                _BuyYaml.ValueChanged += BuyYamlOnValueChanged;
+                BuyYamlOnValueChanged();
             }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake))]
+            public static void Awake_Postfix()
+            {
+                _SellYaml.ValueChanged += SellYamlOnValueChanged;
+                SellYamlOnValueChanged();
+            }
+            //
+            // [HarmonyPostfix]
+            // [HarmonyPatch(typeof(ItemDrop), nameof(ItemDrop.Awake))]
+            // public static void GetValue_Postfix(ItemDrop __instance)
+            // {
+            //     _SellYaml.ValueChanged += SellYamlOnValueChanged;
+            //     foreach (var item in _playerItem.Where(item => __instance.gameObject.name == item.m_prefab))
+            //     {
+            //         __instance.m_itemData.m_shared.m_value = item.m_value;
+            //     }
+            // }
         }
 
-        private static List<TraderYaml> ReadSerializedYml(string str)
+        private static List<TraderYaml> ReadSerializedBuyYml(string str)
         {
             return new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build()
                 .Deserialize<List<TraderYaml>>(str);
         }
+
+        private static List<SellableItem> ReadSerializedSellYml(string str)
+        {
+            return new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build()
+                .Deserialize<List<SellableItem>>(str);
+        }
         
-        private static void CreateTraderYml()
+        private static void CreateBuyYml()
         {
             var traderList = new List<TraderYaml>
             {
@@ -207,10 +286,40 @@ namespace RevampTrader
                     m_requiredGlobalKey = "defeated_goblinking"
                 }
             };
-            using var streamWriter = new StreamWriter(fullYmlPath);
+            using var streamWriter = new StreamWriter(fullBuyYamlPath);
             var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
                 .Build();
             serializer.Serialize(streamWriter, traderList);
+        }
+
+        private static void CreateSellYml()
+        {
+            var sellableList = new List<SellableItem>
+            {
+                new()
+                {
+                    m_prefab = "Amber",
+                    m_value = 5
+                },
+                new()
+                {
+                    m_prefab = "AmberPearl",
+                    m_value = 10
+                },
+                new()
+                {
+                    m_prefab = "Ruby",
+                    m_value = 20
+                },
+                new()
+                {
+                    m_prefab = "SilverNecklace",
+                    m_value = 30
+                }
+            };
+            using var streamWriter = new StreamWriter(fullSellYamlPath);
+            var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+            serializer.Serialize(streamWriter, sellableList);
         }
     }
 }
